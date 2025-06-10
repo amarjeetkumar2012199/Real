2e6a71b85c985f54f3b50fba48fab70b46df96c0
amarjeetkumar2012199 <amarjeetkumar2012199@gmail.com>
2025-06-11 00:15:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import StudentForm from './components/StudentForm';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./Login";
+import AdmissionForm from "./AdmissionForm";
+import Dashboard from "./Dashboard";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow">
-        <h2 className="text-2xl font-bold text-center mb-6">Student Admission Form</h2>
-        <StudentForm />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        
+        {/* Protected Route */}
+        <Route 
+          path="/form" 
+          element={isLoggedIn ? <AdmissionForm /> : <Navigate to="/" />} 
+        />
+        
+        <Route 
+          path="/dashboard" 
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />} 
+        />
+        
+      </Routes>
+    </Router>
   );
 }
 
